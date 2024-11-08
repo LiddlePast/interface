0400a962b682fbd388f12735bda49b565b4f0049
Matvey Makhanov <mtvmakhanov@gmail.com>
2024-11-08 22:42:34 +0700

    Updated colors and form submit event

--- main.js
+++ main.js
@@ -10,10 +10,7 @@ let inputText;
 document.title = documentTitle;
 appMainTitle.textContent = appTitle;
 
-const responses = [
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consequuntur distinctio dolor ex, explicabo facere fugiat hic ipsam ipsum laudantium maiores nobis possimus quas qui repudiandae tenetur totam unde?",
-    "А ещё сторонники тоталитаризма в науке будут призваны к ответу. Также как внедрение современных методик обеспечивает широкому кругу (специалистов) участие в формировании новых принципов формирования материально-технической и кадровой базы.",
-];
+const responses = ["Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto at consequuntur distinctio dolor ex, explicabo facere fugiat hic ipsam ipsum laudantium maiores nobis possimus quas qui repudiandae tenetur totam unde?", "А ещё сторонники тоталитаризма в науке будут призваны к ответу. Также как внедрение современных методик обеспечивает широкому кругу (специалистов) участие в формировании новых принципов формирования материально-технической и кадровой базы.",];
 
 setTimeout(function () {
     modal.classList.add('-translate-y-full');
@@ -25,7 +22,7 @@ function createMessage(inputMessage, userType) {
         message.className = 'message w-full rounded-full flex'
         message.classList.add((userType === 'user') ? 'justify-end' : 'justify-start')
         message.insertAdjacentHTML('beforeend', `
-            <div class="${userType === 'user' ? 'text-end' : 'text-start'} border-2 bg-yellow-100 w-fit max-w-96 p-2 rounded-md">
+            <div class="${userType === 'user' ? 'text-end' : 'text-start'} ${(userType === 'user') ? 'bg-yellow-100' : 'bg-green-100'} border-2 w-fit max-w-96 p-2 rounded-md">
                 <span style="word-wrap: anywhere">${inputMessage}</span>
             </div>
         `)
@@ -39,24 +36,22 @@ function createMessage(inputMessage, userType) {
 function addMessageToChat(inputMessageElement) {
     chatField.insertAdjacentElement('beforeend', inputMessageElement);
     chatField.scrollTo({
-        top: 10000,
-        behavior: "smooth"
+        top: 10000, behavior: "smooth"
     })
 }
 
-sendMessageBtn.addEventListener('click', function (e) {
+document.forms[0].addEventListener('submit', function (e) {
     e.preventDefault();
     inputText = inputField.value;
-    inputField.value = '';
     if (inputText.length > 0) {
+        inputField.value = '';
         createMessage(inputText, 'user')
         generateResponse();
     }
-});
+})
 
 function chooseText() {
     let choice = Math.abs(Math.round(Math.random() * responses.length - 1));
-    console.log(choice)
     return responses[choice];
 }
 
